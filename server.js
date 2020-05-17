fadf44da497b4c22a1bf32ef3a52184ea767baa1
Max Hauser <hauser.m97@gmail.com>
2020-05-17 15:17:03 +0200

    backend huiswerk

--- server.js
+++ server.js
@@ -2,13 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Home pagina!'));
-app.get('/about', (req, res) => res.send('About pagina!'));
-app.get('/contact', (req, res) => res.send('Contact pagina!'));
-app.use(express.static("static"));
-app.use(function (req, res, next) {
-  res.status(404).send("Dit is de 404 pagina.")
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page 
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
+
+// about page 
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+app.use(express.static('public'))
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
+
+// respond with 404 if status === 404
+app.use(function (req, res, next) {
+  res.status(404).send("404 page, Sorry can't find that!")
+})
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));

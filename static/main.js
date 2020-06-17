e057d23df8f1ac7d2d8cd2069e99e6f02891c0f7
Max Hauser <hauser.m97@gmail.com>
2020-06-17 17:05:49 +0100

    added responsiveness

--- static/main.js
+++ static/main.js
@@ -1,48 +1,43 @@
+// popup filter function
 
+const filter_btn = document.querySelector("#toggle_filter");
+const filter = document.querySelector("#filter");
 
-  // popup filter function
+function toggle_filter(event) {
+  event.preventDefault();
+  console.log(filter);
+  filter.classList.toggle("show");
+}
 
-  const filter_btn = document.querySelector("#toggle_filter");
-  const filter = document.querySelector("#filter");
+filter_btn.addEventListener("click", toggle_filter);
 
-  function toggle_filter(event) {
-    event.preventDefault();
-    console.log(filter)
-    filter.classList.toggle('show')
+const age = document.querySelector("#age");
+const age_value = document.querySelector("#age_value");
+
+age.addEventListener("input", e => {
+  const newValue = Number(((age.value - age.min) * 100) / (age.max - age.min));
+  const newPosition = 8 - newValue * 0.5;
+
+  age_value.innerHTML = `<span> ${e.target.value} </span>`;
+  age_value.style.left = `calc(${newValue}% + (${newPosition}px))`;
+});
+
+const distance_input = document.querySelector("#distance");
+const distance_value = document.querySelector("#distance_value");
+
+distance_input.addEventListener("input", e => {
+  console.log(e.target.value);
+  if (e.target.value == 1) {
+    console.log("city");
+    distance_value.innerHTML = `<span>City</span>`;
+    distance_value.style.left = 5 + "%";
+  } else if (e.target.value == 2) {
+    console.log("country");
+    distance_value.innerHTML = `<span>Country</span>`;
+    distance_value.style.left = 45 + "%";
+  } else {
+    console.log("world");
+    distance_value.innerHTML = `<span>World</span>`;
+    distance_value.style.left = 88 + "%";
   }
-
-  filter_btn.addEventListener('click', toggle_filter);
-
-
-  const age = document.querySelector("#age");
-  const age_value = document.querySelector("#age_value");
-
-  age.addEventListener("input", (e) =>{
-
-    const newValue = Number((age.value - age.min) * 100 / (age.max - age.min));
-    const newPosition = 8 - (newValue * 0.5);
-
-    age_value.innerHTML = `<span> ${e.target.value} </span>`;
-    age_value.style.left = `calc(${newValue}% + (${newPosition}px))`;
-  });
-
-
-  const distance_input = document.querySelector("#distance");
-  const distance_value = document.querySelector("#distance_value");
-
-  distance_input.addEventListener("input", (e)=>{
-    console.log(e.target.value)
-    if(e.target.value == 1){
-      console.log("city");
-      distance_value.innerHTML = `<span>City</span>`;
-      distance_value.style.left = 5 + "%";
-    }else if(e.target.value == 2){
-      console.log("country");
-      distance_value.innerHTML = `<span>Country</span>`;
-      distance_value.style.left = 45 + "%";
-      }else{
-        console.log("world");
-        distance_value.innerHTML = `<span>World</span>`;
-        distance_value.style.left = 88 + "%";
-      }
-    });
+});
